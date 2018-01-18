54094173e1c1c3bcb0dc87c5580eadfc46857ee5
tkems <tkems@linux-1.ece.iastate.edu>
2018-01-18 09:03:12 -0600

    Changed dot.c

--- dot.c
+++ dot.c
@@ -1,7 +1,8 @@
 double dot(double a[], double b[], int length) {
 	int i;
-	double sum;   
-	for (i=1; i<= length; i++) { 
+	double sum;
+	sum = 0;   
+	for (i=0; i< length; i++) { 
 		sum = sum + a[i]*b[i];
 		}
 	return sum;
